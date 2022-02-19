2f8aa8cecdead6fd21a08ea39e311b21540f73e2
Metaras Charoenseang <metaras.c@Ku.th>
2022-02-19 16:32:17 +0000

    add prisma

--- src/routes/todos/index.json.ts
+++ src/routes/todos/index.json.ts
@@ -8,7 +8,6 @@ export const get = (request) => {
 export const post = async (request) => {
   const data = await request.request.formData();
   return api(request, {
-    uid: `${Date.now()}`,  // TODO replace this with uid from data base
     created_at: new Date(),
     text: data.get("text").toString(),
     done: false
